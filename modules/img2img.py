2a66b319b53e5c8763e982892139f6c6718ca1ba
Chattiori <127287320+Faildes@users.noreply.github.com>
2023-08-15 11:35:30 +0900

    Update img2img.py

--- modules/img2img.py
+++ modules/img2img.py
@@ -80,7 +80,7 @@ def process_batch(p, input_dir, output_dir, inpaint_mask_dir, args):
 
 def img2img(id_task: str, mode: int, prompt: str, negative_prompt: str, prompt_styles, init_img, sketch, init_img_with_mask, inpaint_color_sketch, inpaint_color_sketch_orig, init_img_inpaint, init_mask_inpaint, steps: int, sampler_index: int, mask_blur: int, mask_alpha: float, inpainting_fill: int, restore_faces: bool, tiling: bool, n_iter: int, batch_size: int, cfg_scale: float, image_cfg_scale: float, denoising_strength: float, seed: int, subseed: int, subseed_strength: float, seed_resize_from_h: int, seed_resize_from_w: int, seed_enable_extras: bool, selected_scale_tab: int, height: int, width: int, scale_by: float, resize_mode: int, inpaint_full_res: bool, inpaint_full_res_padding: int, inpainting_mask_invert: int, img2img_batch_input_dir: str, img2img_batch_output_dir: str, img2img_batch_inpaint_mask_dir: str, override_settings_texts, *args):
     override_settings = create_override_settings_dict(override_settings_texts)
-
+    du = False
     is_batch = mode == 5
 
     if mode == 0:  # img2img
@@ -110,16 +110,27 @@ def img2img(id_task: str, mode: int, prompt: str, negative_prompt: str, prompt_s
         image = None
         mask = None
 
+    if resize_mode == 4:  # down_up
+        image_k = init_img.convert("RGB")
+        i = image_k.size   # current size (height,width)
+        width = image_k.width
+        height = image_k.height
+        i = int(i[0] // scale_by), int(i[1] // scale_by)  # new size
+        image = image_k.resize(i).convert("RGB")
+        mask = None
+        du = True
+        resize_mode = 0
+    
     # Use the EXIF orientation of photos taken by smartphones.
     if image is not None:
         image = ImageOps.exif_transpose(image)
 
-    if selected_scale_tab == 1:
+    if selected_scale_tab == 1 and not du:
         assert image, "Can't scale by because no image is selected"
 
         width = int(image.width * scale_by)
         height = int(image.height * scale_by)
-
+    
     assert 0. <= denoising_strength <= 1., 'can only work with strength in [0.0, 1.0]'
 
     p = StableDiffusionProcessingImg2Img(
