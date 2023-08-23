8564ebd51aa71736aa0caf1f83199cd8c38afcad
Chattiori <127287320+Faildes@users.noreply.github.com>
2023-08-23 12:19:42 +0900

    Update sd_samplers_kdiffusion.py

--- modules/sd_samplers_kdiffusion.py
+++ modules/sd_samplers_kdiffusion.py
@@ -13,6 +13,7 @@ from modules.script_callbacks import AfterCFGCallbackParams, cfg_after_cfg_callb
 samplers_k_diffusion = [
     ('DPM++ 2M Karras', 'sample_dpmpp_2m', ['k_dpmpp_2m_ka'], {'scheduler': 'karras'}),
     ('DPM++ SDE Karras', 'sample_dpmpp_sde', ['k_dpmpp_sde_ka'], {'scheduler': 'karras', "second_order": True, "brownian_noise": True}),
+    ('DPM++ SDE Exponential', 'sample_dpmpp_sde', ['k_dpmpp_sde_ka'], {'scheduler': 'exponential', "second_order": True, "brownian_noise": True}),
     ('DPM++ 2M SDE Exponential', 'sample_dpmpp_2m_sde', ['k_dpmpp_2m_sde_exp'], {'scheduler': 'exponential', "brownian_noise": True}),
     ('DPM++ 2M SDE Karras', 'sample_dpmpp_2m_sde', ['k_dpmpp_2m_sde_ka'], {'scheduler': 'karras', "brownian_noise": True}),
     ('Euler a', 'sample_euler_ancestral', ['k_euler_a', 'k_euler_ancestral'], {"uses_ensd": True}),
@@ -73,11 +74,67 @@ def sample_dpmpp_2m_alt(model, x, sigmas, extra_args=None, callback=None, disabl
         adjustment_factor = 1 + (0.15 * (sigma_progress * sigma_progress)) 
         old_denoised = denoised * adjustment_factor
     return x
+    
+@torch.no_grad()
+def sample_dpmpp_3m_sde_alt(model, x, sigmas, extra_args=None, callback=None, disable=None, eta=1., s_noise=1., noise_sampler=None):
+    """DPM-Solver++(3M) SDE."""
 
+    sigma_min, sigma_max = sigmas[sigmas > 0].min(), sigmas.max()
+    noise_sampler = BrownianTreeNoiseSampler(x, sigma_min, sigma_max) if noise_sampler is None else noise_sampler
+    extra_args = {} if extra_args is None else extra_args
+    s_in = x.new_ones([x.shape[0]])
+
+    denoised_1, denoised_2 = None, None
+    h_1, h_2 = None, None
+
+    for i in trange(len(sigmas) - 1, disable=disable):
+        denoised = model(x, sigmas[i] * s_in, **extra_args)
+        if callback is not None:
+            callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigmas[i], 'denoised': denoised})
+        if sigmas[i + 1] == 0:
+            # Euler method
+            d = to_d(x, sigmas[i], denoised)
+            dt = sigmas[i + 1] - sigmas[i]
+            x = x + d * dt
+        else:
+            t, s = -sigmas[i].log(), -sigmas[i + 1].log()
+            h = s - t
+            h_eta = h * (eta + 1)
+
+            x = torch.exp(-h_eta) * x + (-h_eta).expm1().neg() * denoised
+
+            if h_2 is not None:
+                r0 = h_1 / h
+                r1 = h_2 / h
+                d1_0 = (denoised - denoised_1) / r0
+                d1_1 = (denoised_1 - denoised_2) / r1
+                d1 = d1_0 + (d1_0 - d1_1) * r0 / (r0 + r1)
+                d2 = (d1_0 - d1_1) / (r0 + r1)
+                phi_2 = h_eta.neg().expm1() / h_eta + 1
+                phi_3 = phi_2 / h_eta - 0.5
+                x = x + phi_2 * d1 - phi_3 * d2
+            elif h_1 is not None:
+                r = h_1 / h
+                d = (denoised - denoised_1) / r
+                phi_2 = h_eta.neg().expm1() / h_eta + 1
+                x = x + phi_2 * d
+
+            if eta:
+                x = x + noise_sampler(sigmas[i], sigmas[i + 1]) * sigmas[i + 1] * (-2 * h * eta).expm1().neg().sqrt() * s_noise
+
+        denoised_1, denoised_2 = denoised, denoised_1
+        h_1, h_2 = h, h_1
+    return x
+    
 k_diffusion.sampling.sample_dpmpp_2m_alt = sample_dpmpp_2m_alt
+k_diffusion.sampling.sample_dpmpp_3m_sde_alt = sample_dpmpp_3m_sde_alt
 
 samplers_data_k_diffusion.insert(9, sd_samplers_common.SamplerData('DPM++ 2M alt', lambda model: KDiffusionSampler('sample_dpmpp_2m_alt', model), ['k_dpmpp_2m_alt'], {}))
 samplers_data_k_diffusion.insert(10, sd_samplers_common.SamplerData('DPM++ 2M alt Karras', lambda model: KDiffusionSampler('sample_dpmpp_2m_alt', model), ['k_dpmpp_2m_alt_ka'], {'scheduler': 'karras'}))
+samplers_data_k_diffusion.insert(11, sd_samplers_common.SamplerData('DPM++ 2M alt Exponential', lambda model: KDiffusionSampler('sample_dpmpp_2m_alt', model), ['k_dpmpp_2m_alt_ka'], {'scheduler': 'exponential'}))
+samplers_data_k_diffusion.insert(12, sd_samplers_common.SamplerData('DPM++ 3M SDE alt', lambda model: KDiffusionSampler('sample_dpmpp_3m_sde_alt', model), ['k_dpmpp_3m_sde_alt'], {'discard_next_to_last_sigma': True, "brownian_noise": True}))
+samplers_data_k_diffusion.insert(13, sd_samplers_common.SamplerData('DPM++ 3M SDE alt Karras', lambda model: KDiffusionSampler('sample_dpmpp_3m_sde_alt', model), ['k_dpmpp_3m_sde_alt'], {'scheduler': 'karras','discard_next_to_last_sigma': True, "brownian_noise": True}))
+samplers_data_k_diffusion.insert(14, sd_samplers_common.SamplerData('DPM++ 3M SDE alt Exponential', lambda model: KDiffusionSampler('sample_dpmpp_3m_sde_alt', model), ['k_dpmpp_3m_sde_alt'], {'scheduler': 'exponential','discard_next_to_last_sigma': True, "brownian_noise": True}))
 
 sampler_extra_params = {
     'sample_euler': ['s_churn', 's_tmin', 's_tmax', 's_noise'],
