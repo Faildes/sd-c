3a552462775863b4c574883c5fd7ff694b23b0b0
Chattiori <127287320+Faildes@users.noreply.github.com>
2023-09-04 10:15:50 +0900

    Update lora_block_weight.py

--- extensions/sd-webui-lora-block-weight/scripts/lora_block_weight.py
+++ extensions/sd-webui-lora-block-weight/scripts/lora_block_weight.py
@@ -77,6 +77,8 @@ OUTS:1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1\n\
 OUTALL:1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1\n\
 ALL0.5:0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5"
 
+scriptpath = os.path.dirname(os.path.abspath(__file__))
+
 class Script(modules.scripts.Script):
     def __init__(self):
         self.log = {}
@@ -95,6 +97,7 @@ class Script(modules.scripts.Script):
 
         scriptpath = os.path.dirname(os.path.abspath(__file__))
         path_root = scripts.basedir()
+        
         extpath = os.path.join(scriptpath, "lbwpresets.txt")
         extpathe = os.path.join(scriptpath, "elempresets.txt")
         filepath = os.path.join(path_root,"scripts", "lbwpresets.txt")
@@ -576,11 +579,12 @@ def loradealer(self, prompts,lratios,elementals, extra_network_data = None):
                 lorars.append(ratios)
             if len(called.items) > 3:
                 if syntaxdealer(called.items, "lbwe=",None,3) in elementals:
-                    elements.append(elementals[called.items[3]])
+                    elements.append(elementals[syntaxdealer(called.items, "lbwe=",None,3)])
                 else:
                     elements.append(called.items[3])
             else:
                 elements.append("")
+        if self.isnet: ltype = "nets"
         if len(lorars) > 0: load_loras_blocks(self, lorans,lorars,multipliers,elements,ltype)
 
 def syntaxdealer(items,type1,type2,index): #type "unet=", "x=", "lwbe=" 
@@ -629,7 +633,14 @@ def load_loras_blocks(self, names, lwei,multipliers,elements = [],ltype = "lora"
                 if name == loaded.name:
                     lbw(lycomo.loaded_lycos[l],lwei[n],elements[n])
                     lycomo.loaded_lycos[l].name = lycomo.loaded_lycos[l].name +"_in_LBW_"+ str(round(random.random(),3))
-    
+                    
+    elif "nets" == ltype:
+        import networks as nets
+        for l, loaded in enumerate(nets.loaded_networks):
+            for n, name in enumerate(names):
+                if name == loaded.name:
+                    lbw(nets.loaded_networks[l],lwei[n],elements[n])
+                    nets.loaded_networks[l].name = nets.loaded_networks[l].name +"_in_LBW_"+ str(round(random.random(),3)) 
     try:
         import lora_ctl_network as ctl
         for old,new in oldnew:
