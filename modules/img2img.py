4d2f603b320901ac68ffea98ce8631b93d231e0c
Chattiori <127287320+Faildes@users.noreply.github.com>
2023-09-11 14:15:23 +0900

    Update img2img.py

--- modules/img2img.py
+++ modules/img2img.py
@@ -92,7 +92,10 @@ def img2img(id_task: str, mode: int, prompt: str, negative_prompt: str, prompt_s
     elif mode == 2:  # inpaint
         image, mask = init_img_with_mask["image"], init_img_with_mask["mask"]
         alpha_mask = ImageOps.invert(image.split()[-1]).convert('L').point(lambda x: 255 if x > 0 else 0, mode='1')
-        mask = mask.convert('L').point(lambda x: 255 if x > 128 else 0, mode='1')
+        if mask.mode == 'RGBA' and mask.getextrema()[-1] != (255, 255):
+            mask = mask.split()[-1].convert('L').point(lambda x: 255 if x > 128 else 0, mode='1')
+        else:
+            mask = mask.convert('L').point(lambda x: 255 if x > 128 else 0, mode='1')
         mask = ImageChops.lighter(alpha_mask, mask).convert('L')
         image = image.convert("RGB")
     elif mode == 3:  # inpaint sketch
